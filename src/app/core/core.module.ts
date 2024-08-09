8018c516d48c86a04aef4e23a3d5eccc77dca777
Magadanov <magadanov03@mail.ru>
2024-08-09 23:07:27 +0500

    feat: add footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HeaderComponent, SidenavComponent],
-  exports: [HeaderComponent, SidenavComponent],
+  imports: [CommonModule, HeaderComponent, SidenavComponent, FooterComponent],
+  exports: [HeaderComponent, SidenavComponent, FooterComponent],
 })
 export class CoreModule {}
